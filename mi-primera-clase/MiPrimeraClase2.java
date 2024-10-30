de76c17f83f16427681ab7b763f5b9fb160b35e1
alicia <alicia.ibanez.escudero@gmail.com>
2024-10-30 14:06:04 +0100

    Nombre completo

---
+++ mi-primera-clase/MiPrimeraClase2.java
@@ -0,0 +1,11 @@
+public class MiPrimeraClase2 {
+	
+	public static void main (String args[]) {
+		
+		System.out.println ("Hola Mundo!");
+		System.out.println ("Saludos desde León");
+		System.out.println ("León, 1994");
+		
+	}
+	
+}
---
+++ mi-primera-clase/MiPrimeraClase3.java
@@ -0,0 +1,12 @@
+public class MiPrimeraClase3 {
+	
+	public static void main (String args[]) {
+		
+		System.out.println ("Hola Mundo!");
+		System.out.println ("Saludos desde León");
+		System.out.println ("León, 1994");
+		System.out.println ("La Virgen del Camino, Windows11");
+		
+	}
+	
+}
